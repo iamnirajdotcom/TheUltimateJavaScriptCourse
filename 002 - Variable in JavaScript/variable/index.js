f710671c4255409e78a3857d65f3fcde750e6db9
Niraj <nkgangale@gmail.com>
2022-07-21 14:35:44 +0530

    Add variable file.

---
+++ 002 - Variable in JavaScript/variable/index.js
@@ -0,0 +1,45 @@
+//  <<<<<<<<<<  002 - Variable in JavaScript  >>>>>>>>>>
+
+/*
+    handwritten notes -> https://drive.google.com/drive/folders/1Tz1-Iq9TtHUSgIGeohxK3nxIyFE7ktYI
+
+    Before we start the lecture let's get familiar with the naming convention of javascript files.
+    1] all letter should be lower-case.
+    2] may include underscore(_) or dashes(-) no other punctuation.
+
+    // rules for naming the variable (look in to the notes)
+
+ */
+
+// begin with letter
+let a = 45
+// begin with underscore
+let _num = 45
+// begin with Dollar ($)
+let $num2 = 26
+// Start with capital letter
+let Num = 96
+let num = 45
+
+// num and Num both are different variable  as javascript is case sensitive.
+console.log("Num "+ Num)
+console.log("num "+ num)
+
+console.log(a,_num,$num2,Num)
+
+// let 5first_name = "noble"  // throw error because variable name starting with number is not allowed.
+
+
+// In the next lecture we will look into different ways to declare variable using
+//  let || var
+// We will also learn about the use of the
+//  const
+// for declaring the constants
+
+/*
+    <<<<<<<<<<<< see you in the next module >>>>>>>>>>>>>
+    ------------------------------------------------------
+    |      Credit for notes, and inspiration goes to     |
+    |                  @CodeWithHarry                    |
+    ------------------------------------------------------
+ */
