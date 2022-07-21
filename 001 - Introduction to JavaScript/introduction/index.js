b6bad642a838ffb49770a0254d6ed06e69fadd79
Niraj <nkgangale@gmail.com>
2022-07-21 10:48:00 +0530

    Add index.js file.

---
+++ 001 - Introduction to JavaScript/introduction/index.js
@@ -0,0 +1,12 @@
+// <<<<<<<  001 - Introduction to JavaScript >>>>>>>>
+
+/*
+    we will learn step by step what does things like Console, log, function etc.
+    This is first lecture so just read the handwritten note provided
+
+    handwritten notes -> https://drive.google.com/drive/folders/1RcyvvOAxJtNc_mPLt9g-kTHIMUZ3uD8z
+
+    <<<<<<<<<< see you in the next module >>>>>>>>>>>>>
+*/
+
+console.log("my username in noble niraj")
