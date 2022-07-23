5b70502fad985258b0e03a2f55a99a4bc60ac5fb
Niraj Kumar Gangale <34791044+NobleNiraj@users.noreply.github.com>
2022-07-23 09:37:13 +0530

    Update index.js

--- 001 - Introduction to JavaScript/introduction/index.js
+++ 001 - Introduction to JavaScript/introduction/index.js
@@ -6,21 +6,15 @@
 //JavaScript is the programming language of the Web.
 //JavaScript is easy to learn.
 
-// click on the link to read the intro of javascript.
+// To read the intro of javascript.
 // -> https://www.codingninjas.com/codestudio/guided-paths/basics-of-javascript/content/160175/offering/2096137
 
 console.log("my username in noble niraj")
 
 /*
     we will learn step by step what does things like Console, log etc.
-    This is first lecture so just read the handwritten note provided
 
     handwritten notes -> https://drive.google.com/drive/folders/1RcyvvOAxJtNc_mPLt9g-kTHIMUZ3uD8z
-
-    <<<<<<<<<<<< see you in the next module >>>>>>>>>>>>>
-    ------------------------------------------------------
-    |      Credit for notes, and inspiration goes to     |
-    |                  @CodeWithHarry                    |
-    ------------------------------------------------------
+    
 */
 
